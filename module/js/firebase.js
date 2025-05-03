ef3ebc4b9f33584fd73f3a7e4ac5f6ad1cee674b
Magnus Daa Magnus <magnusdaamagnus@gmail.com>
2025-05-04 00:50:39 +0100

    Update firebase.js

--- module/js/firebase.js
+++ module/js/firebase.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAlF0EJuZfJBTnFc6kqWclB-9v-5CtZPw8",
-    authDomain: "binance-cointracker.firebaseapp.com",
-    databaseURL: "https://binance-cointracker-default-rtdb.firebaseio.com",
-    projectId: "binance-cointracker",
-    storageBucket: "binance-cointracker.appspot.com",
-    messagingSenderId: "864873554352",
-    appId: "1:864873554352:web:7fa92b8948e63843d9b675",
-    measurementId: "G-HP1QJKP14J"
+    apiKey: "AIzaSyA11vXuujZsr37GbHfnezKg0fbVzlj8F7Y",
+	authDomain: "binance-example.firebaseapp.com",
+	databaseURL: "https://binance-example-default-rtdb.firebaseio.com",
+	projectId: "binance-example",
+	storageBucket: "binance-example.firebasestorage.app",
+	messagingSenderId: "1067871213426",
+	appId: "1:1067871213426:web:6596458866c25d55adafeb",
+	measurementId: "G-DWJRFX7XGJ"
 };
