8fa1b2f1eec70c2bc7768c4ec956565178e321c0
Magnus Daa Magnus <magnusdaamagnus@gmail.com>
2025-05-04 15:33:20 +0100

    Update firebase.js

--- module/js/firebase.js
+++ module/js/firebase.js
@@ -1,10 +1,11 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyA11vXuujZsr37GbHfnezKg0fbVzlj8F7Y",
-	authDomain: "binance-example.firebaseapp.com",
-	databaseURL: "https://binance-example-default-rtdb.firebaseio.com",
-	projectId: "binance-example",
-	storageBucket: "binance-example.firebasestorage.app",
-	messagingSenderId: "1067871213426",
-	appId: "1:1067871213426:web:6596458866c25d55adafeb",
-	measurementId: "G-DWJRFX7XGJ"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA11vXuujZsr37GbHfnezKg0fbVzlj8F7Y",
+  authDomain: "binance-example.firebaseapp.com",
+  databaseURL: "https://binance-example-default-rtdb.firebaseio.com",
+  projectId: "binance-example",
+  storageBucket: "binance-example.firebasestorage.app",
+  messagingSenderId: "1067871213426",
+  appId: "1:1067871213426:web:6596458866c25d55adafeb",
+  measurementId: "G-DWJRFX7XGJ"
 };
